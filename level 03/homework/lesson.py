425d257be0a29a60ec3f1444fc4953ff042e2350
wikkooo10 <wikomxeciii@gmail.com>
2024-06-13 21:12:56 +0400

    second commit

---
+++ level 03/homework/lesson.py
@@ -0,0 +1,7 @@
+name = "irakli"
+last_name = "wiklauri"
+age = 14
+hobby = "judo"
+country = "georgia"
+
+print(name, last_name, age, hobby, country)
---
+++ level 03/homework/lesson2.py
@@ -0,0 +1,7 @@
+name = "irakli"
+last_name = "wiklauri"
+age = 14
+hobby = "judo"
+country = "georgia"
+print(name+last_name+str(age)+hobby+country)
+print("i am" +" "+ name+" "+last_name +" "+"i am"+" " + str(age) +"years old"+" " +"my hobby is"+" " +hobby+" "+"i live in"+" "+country)
